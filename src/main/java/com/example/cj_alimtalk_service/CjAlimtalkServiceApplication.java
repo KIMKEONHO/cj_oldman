0dd57e3434701aa1e04f39749d191a2a2c61b5ba
김건호 <jerry6475@bionm.kr>
2026-02-24 10:57:22 +0900

    [ feat ] : 알림톡 기능 v2. spring 스케쥴러 -> 리눅스 크론탭을 사용하여 실행할 수 있게 변경

--- src/main/java/com/example/cj_alimtalk_service/CjAlimtalkServiceApplication.java
+++ src/main/java/com/example/cj_alimtalk_service/CjAlimtalkServiceApplication.java
@@ -2,11 +2,16 @@ package com.example.cj_alimtalk_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableScheduling
-public class CjAlimtalkServiceApplication {
+public class CjAlimtalkServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CjAlimtalkServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CjAlimtalkServiceApplication.class, args);
